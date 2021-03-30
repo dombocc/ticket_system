35b7a08821be0360c18c61678efb27146289ddf1
Dominic <dominicboc@gmail.com>
2021-03-30 12:29:39 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, escape, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('home.html', name = "John")
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
