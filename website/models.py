d2ad8a20661c60fdc6521946221823c2ac844931
Dominic <dominicboc@gmail.com>
2021-03-31 14:41:25 -0400

    added login_out and redirect

--- website/models.py
+++ website/models.py
@@ -1,4 +1,26 @@
-from flask_login import UserMixin, login_manager, LoginManager
+from flask_login import UserMixin 
+from . import db
+from sqlalchemy.sql import func
 
-class User(UserMixin):
-    pass
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+
+
+# login_manager = LoginManager()
+# @login_manager.user_loader
+# def user_loader(email):
+#     if email not in users:
+#         return
+    
+#     user = User()
+#     user.id = email
+#     return user
+
+# @login_manager.request_loader
+# def request_loader(request):
+#     email = request.form.get('email')
+#     if email not in users:
+        # return
--- website/views.py
+++ website/views.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
-def hello():
+@views.route('/home')
+def home():
     return render_template('home.html')
 
