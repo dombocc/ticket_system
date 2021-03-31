2626f4d8103de47b941c6d12c94cebeb265e21c8
Dominic <dominicboc@gmail.com>
2021-03-30 23:04:49 -0400

    fixed login email_pass post req

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('home.html', name = "John")
+    return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        return email + ' ' + password
+    else:
+        return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
