5d05a92aff2cdf0594aeebec54f28c76dee91cec
Dominic <dominicboc@gmail.com>
2021-04-07 16:57:22 -0400

    some work on all_ticket and single_ticket displays

--- website/views.py
+++ website/views.py
@@ -55,25 +55,28 @@ def new_ticket():
 @login_required
 def tickets():
     
-    # tickets = Ticket.query.select_from(Ticket).join(ticket_ticket_status).join(Ticket_Status).first()
-    
-    # tickets = Ticket.query.select_from(Ticket).join(Priority, Ticket.req_priority == Priority.id).first()
-    # tickets = db.session.query(Ticket,Priority).filter_by(Ticket.req_priority == Priority.id).first()
 
-    tickets = Ticket.query.join(Ticket_Status, Ticket.ticket_statuses).first()
-    # tickets = Ticket.query.join(ticket_ticket_status, Ticket.id == ticket_ticket_status.c.ticket_id).join(Ticket_Status, ticket_ticket_status.c.ticket_status_id == Ticket_Status.id).first()
+    tickets = Ticket.query.all()
 
     
-    # return render_template('tickets.html')
-    # return str(tickets.id) + ' ' + str(tickets.title) + ' ' + str(tickets.created_date) + ' ' + str(tickets.overview) + ' ' + str(tickets.spec_requirements) + ' '  + str(tickets.req_priority) + ' ' + str(tickets.ticket_statuses.all()) 
-    # + ' ' + str(tickets.owner_id) 
+    return render_template('tickets.html', tickets=tickets)
+    
 
+    
     # GETS TICKET STATUSES
-    tickets = Ticket.query.join(Ticket_Status, Ticket.ticket_statuses).first()
-    return str(tickets.id) + ' ' + str(tickets.title) + ' ' + str(tickets.ticket_statuses.first().name) + ' ' + str(tickets.title)
+    # tickets = Ticket.query.join(Ticket_Status, Ticket.ticket_statuses).first()
+    # return str(tickets.id) + ' ' + str(tickets.title) + ' ' + str(tickets.ticket_statuses.first().name) + ' ' + str(tickets.title)
 
     # GIVES PRIORITY
     # tickets = Ticket.query.join(Priority, Ticket.req_priority==Priority.id).first()
     # return str(tickets.id) + ' ' + str(tickets.req_priority) + ' ' + str(tickets.priority.priority_decoded)
  
 
+@views.route('/ticket-<ticket_id>', methods=['GET','POST'])
+@login_required
+def ticket_number(ticket_id):
+    
+    ticket = Ticket.query.filter_by(id='1').first()
+
+    # return str(tickets.id) + ' ' + str(tickets.title) + ' ' + str(tickets.ticket_statuses.all()[-1].name) + ' ' + str(tickets.title)
+    return render_template('single_ticket_info.html', ticket=ticket)
