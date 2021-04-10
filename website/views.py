72f471f76595de3dee3b03e1e0777d1d9a2b95c9
Dominic <dominicboc@gmail.com>
2021-04-09 21:46:13 -0400

    added a few feats to sin tic page_ now assign pri

--- website/views.py
+++ website/views.py
@@ -84,23 +84,32 @@ def ticket_number(ticket_id):
     if not ticket:
         return redirect(url_for('views.tickets'))
 
+    # Status dictonary to get status names
     status_dict = {ticket.ticket_statuses.all()[i].id:ticket.ticket_statuses.all()[i].name for i in range(len(ticket.ticket_statuses.all()))}
     
+    # Statuses for the ticket
     statuses = db.session.query(ticket_ticket_status).filter(text('ticket_id='+str(ticket_id))).order_by(ticket_ticket_status.c.time_stamp).all()
 
+    current_ticket_status = statuses[-1][1]
+    
+
+    # Get status list in order from oldest to newest
     ticket_status = []
     for i in range(len(statuses)):
         ticket_status.append([  status_dict[statuses[i][1]]  , statuses[i][2]])
     
+    # Get List of developers
     developers = User.query.filter_by(user_type=1).order_by(User.id).all()
 
-    #Get list of statuses for updates
+    #Get List of statuses for updates
     ticket_statuses = Ticket_Status.query.order_by(Ticket_Status.id).all()
-    
 
-    # return str(developers) + ' ' + str(ticket.assigned_user.id)
+    priorities = Priority.query.all()
     
-    return render_template('single_ticket_info.html', ticket=ticket, ticket_status=ticket_status[::-1], count_status=len(statuses), developers=developers, developer_length = len(developers), ticket_statuses=ticket_statuses)
+    
+    return render_template('single_ticket_info.html', ticket=ticket, ticket_status=ticket_status[::-1], count_status=len(statuses), 
+                developers=developers, developer_length = len(developers), ticket_statuses=ticket_statuses, current_ticket_status=current_ticket_status,
+                priorities = priorities)
     
 
 
@@ -110,22 +119,52 @@ def ticket_number(ticket_id):
 @login_required
 def update_ticket():
     if request.method == 'POST':
-        ticket_id = request.form.get('ticket_id')
+        ticket_id = int(request.form.get('ticket_id'))
         ticket_title = request.form.get('ticket_title')
         ticket_overview = request.form.get('ticket_overview')
         special_requirements = request.form.get('special_requirements')
-        assigned_user_id = request.form.get('assigned_user')
-        updated_status = request.form.get('update_status')
+        assigned_user_id = int(request.form.get('assigned_user'))
+        updated_status = int(request.form.get('update_status'))
+        assigned_priority = int(request.form.get('assigned_priority'))
 
+        # Get current ticket entitiy
         ticket = Ticket.query.filter_by(id=ticket_id).first()
 
+        # update ticket title
         if ticket.title != ticket_title:
             ticket.title = ticket_title
-            return str(ticket.title)
+            return 'title ' + str(ticket_title)
+        
+        # update ticket overview
+        if ticket.overview != ticket_overview:
+            ticket.overview = ticket_overview
+            return 'overview ' + str(ticket_overview)
+        
+        # update ticket special_requirements
+        if ticket.special_requirements != special_requirements:
+            ticket.special_requirements = special_requirements
+            return 'spec_req ' + str(special_requirements)
+        
+        # update ticket assigned_user
+        if ticket.assigned_user.id != assigned_user_id:
+            ticket.assigned_user.id = assigned_user_id
+            return 'assigned ' + str(assigned_user_id)+ str(ticket.assigned_user.id)
 
+        # assign a priority
+        ###########
+        
+        
+        # update ticket status
+        if ticket.ticket_statuses.all()[-1].id != updated_status:
+            new_status = Ticket_Status.query.filter_by(id=updated_status).first()
+            ticket.ticket_statuses.append(new_status)
+            return 'status ' + str(new_status.id) + str(ticket.ticket_statuses.all()[-1].id)
 
+        # Commit Updates
+        # db.session.commit() 
 
-        return str(ticket_id) + ' ' + str(ticket_title) + ' ' + str(ticket_overview) + ' ' + str(special_requirements) + ' ' + str(updated_status) + ' ' + str(assigned_user_id)
-        # return str(ticket.id) + ' ' + str(ticket.title) + ' ' + str(ticket.overview) + ' ' + str(ticket.special_requirements)
+        # return str(ticket_id) + ' ' + str(ticket_title) + ' ' + str(ticket_overview) + ' ' + str(special_requirements) + ' ' + str(updated_status) + ' ' + str(assigned_user_id)
+        return str(ticket.id) + ' ' + str(ticket.title) + ' ' + str(ticket.overview) + ' ' + str(ticket.special_requirements)
+        # return redirect(url_for('views.tickets'))
 
         
